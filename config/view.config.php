1dad587a8948e69f54108e14b51ba36e4d18243b
Malofeykin Andrey <malofeykin@platformasoft.ru>
2015-10-14 12:14:20 +0300

    fix

---
+++ config/view.config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @link https://github.com/old-town/workflow-designer-client
+ * @author  Malofeykin Andrey  <and-rey2@yandex.ru>
+ */
+namespace OldTown\Workflow\Designer\Client;
+
+/** @noinspection PhpIncludeInspection */
+return [
+    'view_manager' => [
+
+        'template_map' => file_exists( __DIR__ . '/../template_map.php') ?  include __DIR__ . '/../template_map.php' : [
+            'old-town/workflow-designer/layout' => __DIR__ . '/view/old-town/workflow-designer/layout.phtml'
+        ],
+        'template_path_stack' => [
+            __DIR__ . '/../view',
+        ]
+    ],
+];
