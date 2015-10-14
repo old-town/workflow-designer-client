a880f7ea9b62f4d5ed09c479a7e7f066d3fbc5bc
Malofeykin Andrey <malofeykin@platformasoft.ru>
2015-10-14 09:01:51 +0300

    sm-69 Подготовить тестовые модули для workflow-designer

---
+++ config/module.config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @link https://github.com/old-town/workflow-designer-client
+ * @author  Malofeykin Andrey  <and-rey2@yandex.ru>
+ */
+namespace OldTown\Workflow\Designer\Client;
+
+
+return [];
---
+++ src/Controller/WorkflowDesignerController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @link https://github.com/old-town/workflow-designer-client
+ * @author  Malofeykin Andrey  <and-rey2@yandex.ru>
+ */
+namespace OldTown\Workflow\Designer\Client\Controller;
+
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+
+/**
+ * Class WorkflowDesignerController
+ *
+ * @package OldTown\Workflow\Designer\Client\Controller
+ */
+class WorkflowDesignerController extends AbstractActionController
+{
+    /**
+     * @return array
+     */
+    public function indexAction()
+    {
+
+    }
+}
